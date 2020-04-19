fae319170dbfcbc37ba4c9c3de1a639135fb35e6
psm8 <43780167+psm8@users.noreply.github.com>
2020-04-19 22:50:59 +0200

    Added some filtering and selection to bookTable

--- SOA_lab3_4_Books/src/main/java/BooksTableBean.java
+++ SOA_lab3_4_Books/src/main/java/BooksTableBean.java
@@ -3,9 +3,7 @@ import model.enumeration.BookType;
 import model.enumeration.Currency;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 import javax.annotation.PostConstruct;
 
@@ -17,14 +15,26 @@ import javax.inject.Named;
 @ViewScoped
 public class BooksTableBean implements Serializable {
 
-    private static final Random RANDOM = new Random();
     private List<Book> books = new ArrayList<>();
+
+    private List<Book> filteredBooks;
+
+    private List<Book> selectedBooks;
 
     @PostConstruct
     public void init() {
-        for (int i = 1; i <= 10; i++) {
-            books.add(new Book(i, "Title" + i, "Author" + i, BookType.randomBookType(), RANDOM.nextInt(90) + 10, Currency.randomCurrency(), RANDOM.nextInt(900) + 100));
+        Random random  = new Random();
+        for (int i = 1; i <= 20; i++) {
+            books.add(new Book(i, "Title" + random.nextInt(50), "Author" + random.nextInt(50), BookType.randomBookType(), random.nextInt(90) + 10, Currency.randomCurrency(), random.nextInt(900) + 100));
         }
+    }
+
+    public BookType[] getBookTypes() {
+        return model.enumeration.BookType.values();
+    }
+
+    public Currency[] geCurrencies() {
+        return model.enumeration.Currency.values();
     }
 
     public List<Book> getBooks() {
@@ -34,5 +44,27 @@ public class BooksTableBean implements Serializable {
     public void setBooks(List<Book> books) {
         this.books = books;
     }
+
+    public List<Book> getFilteredBooks() {
+        return filteredBooks;
+    }
+
+    public void setFilteredBooks(List<Book> filteredBooks) {
+        this.filteredBooks = filteredBooks;
+    }
+
+    public List<Book> getSelectedBooks() {
+        return selectedBooks;
+    }
+
+    public void setSelectedBooks(List<Book> selectedBooks) {
+        this.selectedBooks = selectedBooks;
+    }
+
+/*    public void getMaxPrice(){
+        double maxPrice = Collections.max(this.books, Comparator.comparing(b -> b));
+        return
+
+    }*/
 }
 
